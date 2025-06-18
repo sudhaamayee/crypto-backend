78ed9bf46f2e60f4d0182b08151bfeccc5d15ecc
Ankur Gupta <126963100+7007259Ankur@users.noreply.github.com>
2025-06-18 09:25:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'https://crypto-play.netlify.app/',
+  origin: 'https://crypto-play.netlify.app',
   credentials: true
 }));
 app.use(express.json());
